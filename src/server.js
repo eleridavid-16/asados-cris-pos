91369182d45e9b09c1d8a5a45c762fa1e4923008
eleridavid-16 <arosemenaeleri2002@gmail.com>
2026-07-21 08:55:28 -0500

    Servir frontend en producción

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +32,13 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("Dispositivo desconectado:", socket.id);
   });
+});
+const distPath = path.join(__dirname, "../dist");
+
+app.use(express.static(distPath));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 3001;
