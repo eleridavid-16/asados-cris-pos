2491baeb517095fe8eeef690a69c75a5623eba67
eleridavid-16 <arosemenaeleri2002@gmail.com>
2026-07-22 15:25:39 -0500

    Cambios de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,93 @@
 import { useState, useEffect } from "react";
 import Cocina from "./cocina";
 import { socket } from "./socket";
+import pollo from "./assets/pollo.png";
+import costillas from "./assets/costillas.png";
+import images from "./assets/images.png";
+import pechuga from "./assets/pechuga.png";
+import logoRes from "./assets/logo_res.png";
 
 const productos = [
-  { id: 1, categoria: "Combinaciones", nombre: "1/4 Pollo + 6 Patacones", precio: 2.5 },
-  { id: 2, categoria: "Combinaciones", nombre: "Costilla + 6 Patacones", precio: 3 },
-  { id: 3, categoria: "Combinaciones", nombre: "Chorizo + 6 Patacones", precio: 3 },
-  { id: 4, categoria: "Combinaciones", nombre: "Pechuga extra para combo", precio: 0.4 },
+  {
+    id: 1,
+    categoria: "Combinaciones",
+    nombre: "1/4 Pollo + 6 Patacones",
+    precio: 2.5,
+    imagen: pollo,
+  },
+  {
+    id: 2,
+    categoria: "Combinaciones",
+    nombre: "Costilla + 6 Patacones",
+    precio: 3,
+    imagen: costillas,
+  },
+  {
+    id: 3,
+    categoria: "Combinaciones",
+    nombre: "Chorizo + 6 Patacones",
+    precio: 3,
+    imagen: images,
+  },
+  {
+    id: 4,
+    categoria: "Combinaciones",
+    nombre: "Pechuga extra para combo",
+    precio: 0.4,
+    imagen: pechuga,
+  },
 
-  { id: 5, categoria: "Pollo asado", nombre: "Pollo Asado Entero", precio: 8.5 },
+  {
+    id: 5,
+    categoria: "Pollo asado",
+    nombre: "Pollo Asado Entero",
+    precio: 8.5,
+  },
   { id: 6, categoria: "Pollo asado", nombre: "1/2 Pollo Asado", precio: 4.25 },
   { id: 7, categoria: "Pollo asado", nombre: "1/4 Pollo Asado", precio: 2.25 },
   { id: 8, categoria: "Pollo asado", nombre: "Pechuga", precio: 2.5 },
 
-  { id: 9, categoria: "Costillas y carnes", nombre: "Costilla Entera 16 oz", precio: 9.5 },
-  { id: 10, categoria: "Costillas y carnes", nombre: "Media Costilla", precio: 4.5 },
-  { id: 11, categoria: "Costillas y carnes", nombre: "Cuarto de Costilla 4 oz", precio: 2.25 },
+  {
+    id: 9,
+    categoria: "Costillas y carnes",
+    nombre: "Costilla Entera 16 oz",
+    precio: 9.5,
+  },
+  {
+    id: 10,
+    categoria: "Costillas y carnes",
+    nombre: "Media Costilla",
+    precio: 4.5,
+  },
+  {
+    id: 11,
+    categoria: "Costillas y carnes",
+    nombre: "Cuarto de Costilla 4 oz",
+    precio: 2.25,
+  },
   { id: 12, categoria: "Costillas y carnes", nombre: "1/2 Mixto", precio: 4.5 },
-  { id: 13, categoria: "Costillas y carnes", nombre: "Mixto Entero", precio: 9.5 },
+  {
+    id: 13,
+    categoria: "Costillas y carnes",
+    nombre: "Mixto Entero",
+    precio: 9.5,
+  },
   { id: 14, categoria: "Costillas y carnes", nombre: "Carne Asada", precio: 4 },
 
-  { id: 15, categoria: "Acompañamientos", nombre: "Bollos de Maíz", precio: 1.25 },
+  {
+    id: 15,
+    categoria: "Acompañamientos",
+    nombre: "Bollos de Maíz",
+    precio: 1.25,
+  },
   { id: 16, categoria: "Acompañamientos", nombre: "Patacones", precio: 1.5 },
   { id: 17, categoria: "Acompañamientos", nombre: "Papas Fritas", precio: 1.5 },
-  { id: 18, categoria: "Acompañamientos", nombre: "Papas Horneadas", precio: 2 },
+  {
+    id: 18,
+    categoria: "Acompañamientos",
+    nombre: "Papas Horneadas",
+    precio: 2,
+  },
 
   { id: 19, categoria: "Bebidas", nombre: "Chicheme", precio: 1.5 },
   { id: 20, categoria: "Bebidas", nombre: "Soda", precio: 1.25 },
@@ -44,7 +108,8 @@ const categorias = [
 
 function App() {
   if (window.location.pathname === "/cocina") {
-  return <Cocina />;}
+    return <Cocina />;
+  }
 
   const [vista, setVista] = useState("pos");
   const [categoriaActiva, setCategoriaActiva] = useState("Combinaciones");
@@ -67,7 +132,7 @@ function App() {
         return actual.map((item) =>
           item.id === producto.id
             ? { ...item, cantidad: item.cantidad + 1 }
-            : item
+            : item,
         );
       }
 
@@ -83,16 +148,14 @@ function App() {
 
     setCarrito((actual) =>
       actual.map((item) =>
-        item.id === id
-          ? { ...item, cantidad: nuevaCantidad }
-          : item
-      )
+        item.id === id ? { ...item, cantidad: nuevaCantidad } : item,
+      ),
     );
   };
 
   const total = carrito.reduce(
     (suma, item) => suma + item.precio * item.cantidad,
-    0
+    0,
   );
 
   const enviarPedido = () => {
@@ -113,53 +176,68 @@ function App() {
 
     setPedidos(nuevosPedidos);
 
-    localStorage.setItem(
-      "pedidos",
-      JSON.stringify(nuevosPedidos)
-    );
+    localStorage.setItem("pedidos", JSON.stringify(nuevosPedidos));
 
-    localStorage.setItem(
-      "ultimoNumeroPedido",
-      String(numeroPedido)
-    );
-socket.emit("nuevoPedido", pedidoNuevo);
+    localStorage.setItem("ultimoNumeroPedido", String(numeroPedido));
+    socket.emit("nuevoPedido", pedidoNuevo);
     setCarrito([]);
     setNumeroPedido((actual) => actual + 1);
   };
 
   const eliminarPedido = (numero) => {
-    const nuevosPedidos = pedidos.filter(
-      (pedido) => pedido.numero !== numero
-    );
+    const nuevosPedidos = pedidos.filter((pedido) => pedido.numero !== numero);
 
     setPedidos(nuevosPedidos);
 
-    localStorage.setItem(
-      "pedidos",
-      JSON.stringify(nuevosPedidos)
-    );
+    localStorage.setItem("pedidos", JSON.stringify(nuevosPedidos));
   };
-  
+
   const reiniciarPedidos = () => {
-  const confirmar = window.confirm(
-    "¿Seguro que deseas reiniciar los pedidos? El próximo será el #001."
-  );
+    const confirmar = window.confirm(
+      "¿Seguro que deseas reiniciar los pedidos? El próximo será el #001.",
+    );
 
-  if (!confirmar) {
-    return;
-  }
+    if (!confirmar) {
+      return;
+    }
 
-  localStorage.setItem("ultimoNumeroPedido", "0");
-  localStorage.setItem("pedidos", "[]");
+    localStorage.setItem("ultimoNumeroPedido", "0");
+    localStorage.setItem("pedidos", "[]");
 
-  setNumeroPedido(1);
-  setPedidos([]);
-  setCarrito([]);
-};
+    setNumeroPedido(1);
+    setPedidos([]);
+    setCarrito([]);
+  };
 
   const productosVisibles = productos.filter(
-    (producto) => producto.categoria === categoriaActiva
+    (producto) => producto.categoria === categoriaActiva,
   );
+
+  const ventasPorDia = {};
+
+  pedidos.forEach((pedido) => {
+    // Ajusta esto según el formato de tu fecha
+    const fecha = pedido.fecha.split(" ")[0];
+
+    if (!ventasPorDia[fecha]) {
+      ventasPorDia[fecha] = {
+        total: 0,
+        productos: {},
+        cantidadPedidos: 0,
+      };
+    }
+
+    ventasPorDia[fecha].total += pedido.total;
+    ventasPorDia[fecha].cantidadPedidos++;
+
+    pedido.productos.forEach((producto) => {
+      if (!ventasPorDia[fecha].productos[producto.nombre]) {
+        ventasPorDia[fecha].productos[producto.nombre] = 0;
+      }
+
+      ventasPorDia[fecha].productos[producto.nombre] += producto.cantidad;
+    });
+  });
 
   return (
     <div
@@ -169,17 +247,56 @@ socket.emit("nuevoPedido", pedidoNuevo);
         color: "white",
         padding: "20px",
         fontFamily: "Arial, sans-serif",
+        minHeight: "100vh",
+        backgroundImage: `
+      linear-gradient(
+        rgba(0,0,0,0.5),
+        rgba(0,0,0,0.5)
+      ),
+      url(${logoRes})
+    `,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        color: "white",
+        padding: "20px",
+        fontFamily: "Arial",
       }}
     >
-      <h1 style={{ textAlign: "center", fontSize: "48px",color: "white"}}>
-        Asados Christi
-      </h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "15px",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "48px",
+            color: "white",
+            margin: 0,
+          }}
+        >
+          ASADOS CHRYSTI
+        </h1>
+
+        <img
+          src={logoRes}
+          alt="Logo"
+          style={{
+            width: "125px",
+            height: "100px",
+          }}
+        />
+      </div>
 
       <div
         style={{
           display: "flex",
           gap: "10px",
           marginBottom: "20px",
+          justifyContent: "center",
         }}
       >
         <button
@@ -211,20 +328,31 @@ socket.emit("nuevoPedido", pedidoNuevo);
         >
           PEDIDOS ENVIADOS ({pedidos.length})
         </button>
+        <button 
+        style={{
+            padding: "14px 22px",
+            backgroundColor: vista === "reporte" ? "#d32f2f" : "#444",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            fontSize: "17px",
+            cursor: "pointer",
+          }}
+        onClick={() => setVista("reporte")}>REPORTE DIARIO</button>
         <button
-  onClick={reiniciarPedidos}
-  style={{
-    background: "#d32f2f",
-    color: "white",
-    border: "none",
-    borderRadius: "5px",
-    padding: "14px 20px",
-    fontSize: "17px",
-    cursor: "pointer",
-  }}
->
-  🔄 REINICIAR PEDIDOS
-</button>
+          onClick={reiniciarPedidos}
+          style={{
+            background: "#d32f2f",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            padding: "14px 20px",
+            fontSize: "17px",
+            cursor: "pointer",
+          }}
+        >
+          🔄 REINICIAR PEDIDOS
+        </button>
       </div>
 
       {vista === "pos" && (
@@ -246,13 +374,8 @@ socket.emit("nuevoPedido", pedidoNuevo);
                   borderRadius: "5px",
                   border: "2px solid #ff9800",
                   backgroundColor:
-                    categoriaActiva === categoria
-                      ? "#d32f2f"
-                      : "transparent",
-                  color:
-                    categoriaActiva === categoria
-                      ? "white"
-                      : "#ff9800",
+                    categoriaActiva === categoria ? "#d32f2f" : "transparent",
+                  color: categoriaActiva === categoria ? "white" : "#ff9800",
                   fontSize: "16px",
                   cursor: "pointer",
                 }}
@@ -273,8 +396,7 @@ socket.emit("nuevoPedido", pedidoNuevo);
             <div
               style={{
                 display: "grid",
-                gridTemplateColumns:
-                  "repeat(auto-fit, minmax(210px, 1fr))",
+                gridTemplateColumns: "repeat(auto-fit, minmax(210px, 1fr))",
                 gap: "15px",
               }}
             >
@@ -289,7 +411,12 @@ socket.emit("nuevoPedido", pedidoNuevo);
                     textAlign: "center",
                   }}
                 >
-                  <h2 style={{ minHeight: "58px" }}>
+                  <img
+                    src={producto.imagen}
+                    alt={producto.imagen}
+                    style={{ width: "50%", borderRadius: "10px" }}
+                  />
+                  <h2 style={{ minHeight: "5px", color: "black" }}>
                     {producto.nombre}
                   </h2>
 
@@ -324,7 +451,13 @@ socket.emit("nuevoPedido", pedidoNuevo);
                 padding: "18px",
               }}
             >
-              <h1 style={{ textAlign: "center" }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  color: "black",
+                  fontSize: "50px",
+                }}
+              >
                 Pedido #{String(numeroPedido).padStart(3, "0")}
               </h1>
 
@@ -345,47 +478,58 @@ socket.emit("nuevoPedido", pedidoNuevo);
                   >
                     <strong>{item.nombre}</strong>
 
-                    <p>
-                      ${item.precio.toFixed(2)} cada uno
-                    </p>
+                    <p>${item.precio.toFixed(2)} cada uno</p>
 
                     <div
                       style={{
                         display: "flex",
                         alignItems: "center",
+                        justifyContent: "center",
                         gap: "12px",
                       }}
                     >
                       <button
                         onClick={() =>
-                          cambiarCantidad(
-                            item.id,
-                            item.cantidad - 1
-                          )
+                          cambiarCantidad(item.id, item.cantidad - 1)
                         }
                         style={{
-                          padding: "10px 18px",
-                          fontSize: "20px",
+                          width: "40px",
+                          height: "40px",
+                          borderRadius: "50%",
+                          border: "none",
+                          backgroundColor: "#424242",
+                          color: "white",
+                          fontSize: "24px",
+                          fontWeight: "bold",
                           cursor: "pointer",
                         }}
                       >
                         −
                       </button>
 
-                      <strong style={{ fontSize: "22px" }}>
+                      <strong
+                        style={{
+                          fontSize: "22px",
+                          minWidth: "30px",
+                          textAlign: "center",
+                        }}
+                      >
                         {item.cantidad}
                       </strong>
 
                       <button
                         onClick={() =>
-                          cambiarCantidad(
-                            item.id,
-                            item.cantidad + 1
-                          )
+                          cambiarCantidad(item.id, item.cantidad + 1)
                         }
                         style={{
-                          padding: "10px 18px",
-                          fontSize: "20px",
+                          width: "40px",
+                          height: "40px",
+                          borderRadius: "50%",
+                          border: "none",
+                          backgroundColor: "#ff6f00",
+                          color: "white",
+                          fontSize: "24px",
+                          fontWeight: "bold",
                           cursor: "pointer",
                         }}
                       >
@@ -393,15 +537,18 @@ socket.emit("nuevoPedido", pedidoNuevo);
                       </button>
                     </div>
 
-                    <p>
-                      Subtotal: $
-                      {(item.cantidad * item.precio).toFixed(2)}
-                    </p>
+                    <p>Subtotal: ${(item.cantidad * item.precio).toFixed(2)}</p>
                   </div>
                 ))
               )}
 
-              <h1 style={{ textAlign: "right" }}>
+              <h1
+                style={{
+                  textAlign: "center",
+                  color: "black",
+                  fontSize: "50px",
+                }}
+              >
                 Total: ${total.toFixed(2)}
               </h1>
 
@@ -432,47 +579,105 @@ socket.emit("nuevoPedido", pedidoNuevo);
           {pedidos.length === 0 ? (
             <p>No hay pedidos enviados.</p>
           ) : (
-            [...pedidos].reverse().map((pedido) => (
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+                gap: "20px",
+                marginTop: "20px",
+              }}
+            >
+              {[...pedidos].reverse().map((pedido) => (
+                <div
+                  key={pedido.numero}
+                  style={{
+                    backgroundColor: "white",
+                    color: "black",
+                    padding: "18px",
+                    borderRadius: "8px",
+                  }}
+                >
+                  <h2 style={{ textAlign: "center", color: "black" }}>
+                    Pedido # {String(pedido.numero).padStart(3, "0")}
+                  </h2>
+
+                  <p>{pedido.fecha}</p>
+
+                  {pedido.productos.map((producto) => (
+                    <p key={producto.id}>
+                      {producto.cantidad} × {producto.nombre}
+                    </p>
+                  ))}
+
+                  <h3>Total: ${pedido.total.toFixed(2)}</h3>
+
+                  <button
+                    onClick={() => eliminarPedido(pedido.numero)}
+                    style={{
+                      padding: "10px 18px",
+                      backgroundColor: "#c62828",
+                      color: "white",
+                      border: "none",
+                      borderRadius: "4px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    ELIMINAR
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+
+      {vista === "reporte" && (
+        <div>
+          <h1
+            style={{
+              textAlign: "center",
+              marginBottom: "30px",
+            }}
+          >
+            Reporte Diario de Ventas
+          </h1>
+
+          {Object.entries(ventasPorDia)
+            .reverse()
+            .map(([fecha, datos]) => (
               <div
-                key={pedido.numero}
+                key={fecha}
                 style={{
                   backgroundColor: "white",
                   color: "black",
-                  padding: "18px",
-                  borderRadius: "8px",
-                  marginBottom: "15px",
+                  padding: "20px",
+                  borderRadius: "10px",
+                  marginBottom: "20px",
                 }}
               >
-                <h2>
-                  Pedido #{String(pedido.numero).padStart(3, "0")}
-                </h2>
+                <h2 style={{ color: "black" }}>{fecha}</h2>
 
-                <p>{pedido.fecha}</p>
+                <p>
+                  <strong>Total vendido:</strong>
+                  {" $"}
+                  {datos.total.toFixed(2)}
+                </p>
 
-                {pedido.productos.map((producto) => (
-                  <p key={producto.id}>
-                    {producto.cantidad} × {producto.nombre}
+                <p>
+                  <strong>Pedidos realizados:</strong> {datos.cantidadPedidos}
+                </p>
+
+                <hr />
+
+                <h3>Productos vendidos</h3>
+
+                {Object.entries(datos.productos).map(([nombre, cantidad]) => (
+                  <p key={nombre}>
+                    {nombre}: {cantidad}
                   </p>
                 ))}
-
-                <h3>Total: ${pedido.total.toFixed(2)}</h3>
-
-                <button
-                  onClick={() => eliminarPedido(pedido.numero)}
-                  style={{
-                    padding: "10px 18px",
-                    backgroundColor: "#c62828",
-                    color: "white",
-                    border: "none",
-                    borderRadius: "4px",
-                    cursor: "pointer",
-                  }}
-                >
-                  ELIMINAR
-                </button>
               </div>
-            ))
-          )}
+            ))}
         </div>
       )}
     </div>
